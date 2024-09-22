1f452bb3466492001f442db2a3af2d262c438985
surajislam <154444093+surajislam@users.noreply.github.com>
2024-09-23 02:14:16 +0530

    Update zowner.py

--- BrandrdXMusic/plugins/tools/zowner.py
+++ BrandrdXMusic/plugins/tools/zowner.py
@@ -17,7 +17,7 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "ƨσʋяcɛ", url=f"https://github.com/WCGKING/BrandrdXMusic"
+                        "ƨσʋяcɛ", url=f"SORRY BROO NHI NILA GA "
                     )
                 ]
             ]
@@ -34,7 +34,7 @@ async def clones(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "ƨσʋяcɛ", url=f"https://github.com/WCGKING/BrandrdXMusic"
+                        "ƨσʋяcɛ", url=f"SORRY BRO NHI MILAGA "
                     )
                 ]
             ]
